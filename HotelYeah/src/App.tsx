3abb1c2d712bca26ff8f3df024ab7840b5f776a3
AldousWatts <charles.brough2@gmail.com>
2020-12-29 17:40:24 -0600

    comment out material UI dark theme (causes issues for now)

--- HotelYeah/src/App.tsx
+++ HotelYeah/src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import AuthRoutes from "./routing/AuthRoutes";
-// import '../src/App.css'
+import './App.css'
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Routes from "./routing/Routes";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
-  palette: {
-    type: "dark",
-  },
+  // palette: {
+  //   type: "dark",
+  // },
 });
 
 const App = () => {
