8db74d36f9d692348e1647fd41e870c8e2225463
Argenis Dominguez <argenisdominguez@Argeniss-MacBook-Pro.local>
2020-12-29 14:45:12 -0500

    added material-ui-react

---
+++ HotelYeah/src/util/APIConstants.tsx
@@ -0,0 +1,7 @@
+export const USERNAME = 'revature';
+export const PASSWORD = '$2a$04$VL1atMwHZONSNJQWdSPexe1rRqgLMzU4mgIMBqcqup7VC8odLfjAq';
+export const BASE_URL = 'http://3.139.235.28:8080/hotelbooking-1.0.0';
+
+let headers = new Headers();
+headers.append('Authorization', 'Basic ' + btoa(USERNAME + ":" + PASSWORD));
+export const HEADERS = headers;
